c04e8f39eea94a3060845d948d1f55bb1d98a261
Alejandro Etchegoyen <aetchegoyen@cristalmedia.net>
2020-05-01 19:21:46 +0200

    no message

--- resources/views/Public/Staygig/ViewEvent/Partials/EventOrganiserSection.blade.php
+++ resources/views/Public/Staygig/ViewEvent/Partials/EventOrganiserSection.blade.php
@@ -1,6 +1,6 @@
 <div class="flex-1 bg-white rounded-lg overflow-hidden shadow-lg">
     <a href="#" class="flex flex-wrap no-underline hover:no-underline">
-        <div class="p-4 w-full" style="background-color: {{$event->organiser->page_header_bg_color}};">
+        <div class="p-4 w-full mb-4 text-center" style="background-color: {{$event->organiser->page_header_bg_color}};">
             <img src="{{URL::to($event->organiser->full_logo_path)}}" class="object-contain rounded-t pb-6">
         </div>
         <p class="w-full text-gray-600 text-xs md:text-sm px-6">Organizado por</p>
